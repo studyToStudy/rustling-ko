776ebd67a0de625f2fa6aee0f3d98857e3a8c8f7
studyhard <leejunghoo515@naver.com>
2023-07-19 15:25:31 +0900

    functions1 Solved

---
+++ src/functions/functions1.rs
@@ -0,0 +1,14 @@
+// functions1.rs
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a hint.
+
+/*
+    빈 함수를 호출하고 main에서 실행한다.
+*/
+
+fn call_me() {
+
+}
+
+fn main() {
+    call_me();
+}
