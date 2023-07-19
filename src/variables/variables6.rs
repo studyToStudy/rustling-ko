c61de26694086cac26f74bfbc67a9d71d77d27c6
studyhard <leejunghoo515@naver.com>
2023-07-19 15:10:50 +0900

    variables6 Solve

---
+++ src/variables/variables6.rs
@@ -0,0 +1,12 @@
+// variables6.rs
+// Execute `rustlings hint variables6` or use the `hint` watch subcommand for a hint.
+
+/*
+    const를 선언할때는 const 키워드를 사용하고 변수명은 대문자로만 구성한다.
+    그리고 항상 타입을 명시해줘야 한다.
+*/
+
+const NUMBER: i32 = 3;
+fn main() {
+    println!("Number {}", NUMBER);
+}
