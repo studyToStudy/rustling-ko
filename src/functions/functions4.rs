680026f882c9773f538192cd8850a066eee437bc
studyhard <leejunghoo515@naver.com>
2023-07-19 15:31:11 +0900

    functions4 Solve

---
+++ src/functions/functions4.rs
@@ -0,0 +1,30 @@
+// functions4.rs
+// Execute `rustlings hint functions4` or use the `hint` watch subcommand for a hint.
+
+// This store is having a sale where if the price is an even number, you get
+// 10 Rustbucks off, but if it's an odd number, it's 3 Rustbucks off.
+// (Don't worry about the function bodies themselves, we're only interested
+// in the signatures for now. If anything, this is a good way to peek ahead
+// to future exercises!)
+
+/*
+    Rust에서는 Expression이 값을 반환한다.
+    sale_price의 경우 price - 10이 값을 반환하는데 i32의 값을 리턴한다고 명시해주자.
+*/
+
+fn main() {
+    let original_price = 51;
+    println!("Your sale price is {}", sale_price(original_price));
+}
+
+fn sale_price(price: i32) -> i32 {
+    if is_even(price) {
+        price - 10
+    } else {
+        price - 3
+    }
+}
+
+fn is_even(num: i32) -> bool {
+    num % 2 == 0
+}
