1aa4ce2ccf7b225afff5ab6004d67196be880840
studyhard <leejunghoo515@naver.com>
2023-07-19 14:32:28 +0900

    rustling first

---
+++ target/debug/deps/rustlings-25d46eab7eca6a2f.d
@@ -0,0 +1,5 @@
+/Users/junghoo515/Desktop/rustlings/rustlings/target/debug/deps/rustlings-25d46eab7eca6a2f.rmeta: src/main.rs
+
+/Users/junghoo515/Desktop/rustlings/rustlings/target/debug/deps/rustlings-25d46eab7eca6a2f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rustlings-96396a8a6b273357.d
@@ -0,0 +1,5 @@
+/Users/junghoo515/Desktop/rustlings/rustlings/target/debug/deps/rustlings-96396a8a6b273357.rmeta: src/main.rs
+
+/Users/junghoo515/Desktop/rustlings/rustlings/target/debug/deps/rustlings-96396a8a6b273357.d: src/main.rs
+
+src/main.rs:
