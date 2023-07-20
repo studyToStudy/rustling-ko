8f547292192ee2703752ed9a9929a8a8a8e5addb
studyhard <leejunghoo515@naver.com>
2023-07-20 23:47:58 +0900

    strings4 Solve

---
+++ src/strings/strings4.rs
@@ -0,0 +1,33 @@
+// strings4.rs
+
+// Ok, here are a bunch of values-- some are `String`s, some are `&str`s. Your
+// task is to call one of these two functions on each value depending on what
+// you think each value is. That is, add either `string_slice` or `string`
+// before the parentheses on each line. If you're right, it will compile!
+// No hints this time!
+
+/*
+    어떤 상황에서 String인지 혹은 &str인지 구분하고 확인할 수 있었던 문제
+    기본적으로 "string"은 &str이고 String::from() 혹은 .to_string()은 String 타입인것을
+    확실하게 알고가자.
+*/
+
+fn string_slice(arg: &str) {
+    println!("{}", arg);
+}
+fn string(arg: String) {
+    println!("{}", arg);
+}
+
+fn main() {
+    string_slice("blue");
+    string("red".to_string());
+    string(String::from("hi"));
+    string("rust is fun!".to_owned());
+    string("nice weather".into());
+    string(format!("Interpolation {}", "Station"));
+    string_slice(&String::from("abc")[0..1]);
+    string_slice("  hello there ".trim());
+    string("Happy Monday!".to_string().replace("Mon", "Tues"));
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());
+}
